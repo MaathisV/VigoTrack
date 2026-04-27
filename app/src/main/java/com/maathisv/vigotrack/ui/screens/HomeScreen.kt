534e69af086f2b13c485148f30f5c19740afdecf
MaathisV <mvadez1@gmail.com>
2026-04-27 14:59:38 +0200

    UI : Refactor, externalize components

--- app/src/main/java/com/maathisv/vigotrack/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/maathisv/vigotrack/ui/screens/HomeScreen.kt
@@ -1,22 +1,19 @@
 package com.maathisv.vigotrack.ui.screens
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.maathisv.vigotrack.models.ActivitySession
 import com.maathisv.vigotrack.models.ConnectionState
+import com.maathisv.vigotrack.ui.components.ActivityCard
+import com.maathisv.vigotrack.ui.components.CreateActivityCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -121,55 +118,5 @@ fun HomeScreen(viewModel: HomeViewModel) {
                 showConnectionDialog = false
             }
         )
-    }
-}
-
-// --- UI Components to keep the code clean ---
-
-@Composable
-fun CreateActivityCard(onClick: () -> Unit) {
-    Card(
-        modifier = Modifier
-            .height(120.dp)
-            .clickable { onClick() },
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.secondaryContainer
-        )
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = "Add Activity",
-                modifier = Modifier.size(40.dp)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text("New Session", fontWeight = FontWeight.Bold)
-        }
-    }
-}
-
-@Composable
-fun ActivityCard(session: ActivitySession) {
-    Card(
-        modifier = Modifier.height(120.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = session.name,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Medium
-            )
-        }
     }
 }
