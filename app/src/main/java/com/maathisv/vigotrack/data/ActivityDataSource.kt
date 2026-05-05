0fd24c8a2f91a31e5bb5cf1f09310772741eb0c2
MaathisV <mvadez1@gmail.com>
2026-05-05 15:13:22 +0200

    Data: add mappers and activity data source
    Implements entity-domain mapping and updates data source to use new DAO methods.

--- app/src/main/java/com/maathisv/vigotrack/data/ActivityDataSource.kt
+++ app/src/main/java/com/maathisv/vigotrack/data/ActivityDataSource.kt
@@ -1,6 +1,7 @@
 package com.maathisv.vigotrack.data
 
 import com.maathisv.vigotrack.data.dao.ActivityDao
+import com.maathisv.vigotrack.data.mappers.*
 import com.maathisv.vigotrack.models.ActivitySession
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -10,19 +11,26 @@ interface ActivityDataSource {
     suspend fun insertActivity(activity: ActivitySession)
     suspend fun updateActivity(activity: ActivitySession)
     suspend fun deleteActivity(id: String)
+    suspend fun insertLink(activityId: String, link: ActivitySession.ActivityLink)
 }
 
 class RoomActivityDataSource(private val dao: ActivityDao) : ActivityDataSource {
-    override fun getAllActivities(): Flow<List<ActivitySession>> = dao.getAllActivities().map {  ->
-        entities.map { it.toDomainModel() }
-    }
+
+    override fun getAllActivities(): Flow<List<ActivitySession>> =
+        dao.getActivitiesWithLinks().map { poos ->
+            poos.map { it.toDomain() }
+        }
 
     override suspend fun insertActivity(activity: ActivitySession) {
-        dao.insert(activity.toEntity())
+        dao.insertActivity(activity.toEntity())
     }
 
     override suspend fun updateActivity(activity: ActivitySession) {
-        dao.update(activity.toEntity())
+        dao.updateActivity(activity.toEntity())
+    }
+
+    override suspend fun insertLink(activityId: String, link: ActivitySession.ActivityLink) {
+        dao.insertLink(link.toEntity(activityId))
     }
 
     override suspend fun deleteActivity(id: String) = dao.deleteById(id)
