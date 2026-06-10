df2787c29d4186efc2c67a6f3a409a0f46ad1846
MaathisV <mvadez1@gmail.com>
2026-06-10 14:24:13 +0200

    Add Duplicate Bilan Types

--- app/src/main/java/com/maathisv/vigotrack/models/ActivityType.kt
+++ app/src/main/java/com/maathisv/vigotrack/models/ActivityType.kt
@@ -7,8 +7,12 @@ enum class ActivityType(val displayName: String, val category: ActivityCategory)
     RENFORCEMENT("Renforcement", ActivityCategory.ACTIVITE),
     PISCINE("Piscine", ActivityCategory.ACTIVITE),
     TDM6("TDM6", ActivityCategory.BILAN),
-    _10M("10m", ActivityCategory.BILAN),
-    REPOS("Repos", ActivityCategory.BILAN)
+    _10M_1("10m 1", ActivityCategory.BILAN),
+    _10M_2("10m 2", ActivityCategory.BILAN),
+    _10M_3("10m 3", ActivityCategory.BILAN),
+    _10M_4("10m 4", ActivityCategory.BILAN),
+    REPOS_1("Repos 1", ActivityCategory.BILAN),
+    REPOS_2("Repos 2", ActivityCategory.BILAN)
 }
 
 enum class ActivityCategory(val displayName: String) {
